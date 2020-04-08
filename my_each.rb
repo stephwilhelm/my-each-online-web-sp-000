358473a4d9a00052a92262d96d82602f239ddc6d
Steph W <wilhelm.steph@gmail.com>
2020-04-08 15:17:39 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  empty_array = []
+  i = 0
+
+  my_each(collection) do |i|
+    return i
+
+  end
+    while i < collection.length
+      yield(collection[i])
+      i = i + 1
+    end
+    collection
 end
